add1a5682b64bd6f329085bc058c44b4dbf30287
Pedro Henrique <pedrovasalmeida@gmail.com>
2020-06-03 13:09:52 -0300

    criação da rota de listagem de itens

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,10 +1,23 @@
 import express from "express";
+import knex from "./database/connection";
 
 /** Router */
 const routes = express.Router();
 
-routes.get("/", (request, response) => {
-  return response.json({ message: "Hello World" });
+/** Listagem de todos os itens */
+routes.get("/items", async (request, response) => {
+  // SELECT * FROM 'ITEMS'
+  const items = await knex("items").select("*");
+
+  /** Transformação dos dados */
+  const serializedItems = items.map((item) => {
+    return {
+      title: item.title,
+      image_url: `http://localhost:3333/uploads/${item.image}`,
+    };
+  });
+
+  return response.json(serializedItems);
 });
 
 export default routes;
