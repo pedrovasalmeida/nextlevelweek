59e1c93eaff03e56e07bba584cf79068cf238bdb
Pedro Henrique <pedrovasalmeida@gmail.com>
2020-06-03 10:47:02 -0300

    continuação do back-end

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,18 +7,48 @@ const port: number = 3333;
 /** Instancia o servidor */
 const app = express();
 
+app.use(express.json());
+
 /** Rotas */
+
+// Rota: Endereço completo da requisição
+// Recurso: A entidade acessada do sistema. Ex: /users
+
+// GET: Busca uma ou mais informações no banco;
+// POST: Cria uma nova informação no banco;
+// PUT: Atualiza uma informação no banco;
+// DELETE: Apaga uma informação do banco;
+
+// Request Params: parâmetros que vem na própria rota e que identificam
+// um recurso;
+
+// Query Params: parâmetros (geralmente) opcionais;
+
+const users = ["Pedro", "teste"];
+
 app.get("/users", (request, response) => {
   console.log("Lista de usuários:");
 
-  return response.json([
-    "Pedro",
-    "Henrique",
-    "de",
-    "Vasconcellos",
-    "Almeida",
-    ";",
-  ]);
+  return response.json(users);
+});
+
+app.get("/users/:id", (request, response) => {
+  const id = Number(request.params.id);
+
+  if (id >= users.length) return response.json({ erro: "ID não existente!" });
+
+  const user = users[id];
+
+  return response.json(user);
+});
+
+app.post("/users", (request, response) => {
+  const user = {
+    name: "Pedroa",
+    email: "pedro@contato.com",
+  };
+
+  return response.json(user);
 });
 
 /** Escuta na porta 3333 */
