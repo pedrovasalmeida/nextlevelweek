50bbb0d1cbb37d687aed4f87cf21370d28c7e25a
Pedro Henrique <pedrovasalmeida@gmail.com>
2020-06-03 13:55:24 -0300

    configuração das transações e rota de criação de ponto de coleta

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { request, response } from "express";
 import knex from "./database/connection";
 
 /** Router */
@@ -12,12 +12,55 @@ routes.get("/items", async (request, response) => {
   /** Transformação dos dados */
   const serializedItems = items.map((item) => {
     return {
+      id: item.id,
       title: item.title,
       image_url: `http://localhost:3333/uploads/${item.image}`,
     };
   });
 
   return response.json(serializedItems);
+});
+
+/** Cadastro de ponto de coleta */
+routes.post("/collect_points", async (request, response) => {
+  const {
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+    items,
+  } = request.body;
+
+  // Transações, impedem que uma query seja executada em sequência,
+  // caso ocorra erro com alguma outra query
+  const trx = await knex.transaction();
+
+  const insertedIds = await trx("collect_points").insert({
+    image: "image-fake",
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+  });
+
+  const point_id = insertedIds[0];
+
+  const pointItems = items.map((item_id: Number) => {
+    return {
+      item_id,
+      point_id: insertedIds[0],
+    };
+  });
+
+  await trx("point_items").insert(pointItems);
+
+  return response.json({ sucess: true });
 });
 
 export default routes;
