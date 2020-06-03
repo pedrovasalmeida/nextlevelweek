e030fed3668f30f6b063b7e863b1739bf526211f
Pedro Henrique <pedrovasalmeida@gmail.com>
2020-06-03 10:57:51 -0300

    continuando servidor

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,46 +7,24 @@ const port: number = 3333;
 /** Instancia o servidor */
 const app = express();
 
+const users = ["Pedro", "Henrique", "de", "Vasconcellos", "Almeida"];
+
+/** Permite o uso de JSON pelo Express */
 app.use(express.json());
 
-/** Rotas */
-
-// Rota: Endereço completo da requisição
-// Recurso: A entidade acessada do sistema. Ex: /users
-
-// GET: Busca uma ou mais informações no banco;
-// POST: Cria uma nova informação no banco;
-// PUT: Atualiza uma informação no banco;
-// DELETE: Apaga uma informação do banco;
-
-// Request Params: parâmetros que vem na própria rota e que identificam
-// um recurso;
-
-// Query Params: parâmetros (geralmente) opcionais;
-
-const users = ["Pedro", "teste"];
-
 app.get("/users", (request, response) => {
-  console.log("Lista de usuários:");
-
   return response.json(users);
 });
 
 app.get("/users/:id", (request, response) => {
   const id = Number(request.params.id);
 
-  if (id >= users.length) return response.json({ erro: "ID não existente!" });
+  if (id > users.length - 1)
+    return response.json({
+      error: `ID não existe. O último ID é ${users.length - 1}`,
+    });
 
   const user = users[id];
-
-  return response.json(user);
-});
-
-app.post("/users", (request, response) => {
-  const user = {
-    name: "Pedroa",
-    email: "pedro@contato.com",
-  };
 
   return response.json(user);
 });
