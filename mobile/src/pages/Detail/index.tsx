8b969fdd25a0a4a1602a0e9abb0667dc48cf2f29
Pedro Henrique <pedrovasalmeida@gmail.com>
2020-06-07 05:13:14 -0300

    finalização da tela de detalhes; implementação do deep link whatsapp

--- mobile/src/pages/Detail/index.tsx
+++ mobile/src/pages/Detail/index.tsx
@@ -1,8 +1,10 @@
-import React from "react";
-import { View, TouchableOpacity, Text, Image } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, TouchableOpacity, Text, Image, Linking } from "react-native";
 import { Feather as Icon, FontAwesome } from "@expo/vector-icons";
-import { useNavigation } from "@react-navigation/native";
+import { useNavigation, useRoute } from "@react-navigation/native";
 import { RectButton } from "react-native-gesture-handler";
+
+import api from "../../services/api";
 
 import styles from "./styles";
 
@@ -12,53 +14,104 @@ interface Item {
   image_url: string;
 }
 
+interface Params {
+  point_id: number;
+}
+
+interface Data {
+  point: {
+    image: string;
+    name: string;
+    email: string;
+    whatsapp: string;
+    city: string;
+    uf: string;
+  };
+
+  items: {
+    id: number;
+    image: string;
+    title: string;
+  }[];
+}
+
 const Detail = () => {
   const navigation = useNavigation();
+  const route = useRoute();
+
+  const [data, setData] = useState<Data>({} as Data);
+  const routeParams = route.params as Params;
+
+  async function loadPoint() {
+    await api.get(`points/${routeParams.point_id}`).then((response) => {
+      setData(response.data);
+    });
+  }
+
+  useEffect(() => {
+    loadPoint();
+  }, []);
+
+  /** envio de email */
+  // function handleEmail() {
+
+  // }
+  function handleWhatsapp() {
+    Linking.openURL(
+      `whatsapp://send?phone=${data.point.whatsapp}&text=Tenho interesse sobre coleta de resíduos...`
+    );
+  }
 
   /** navegação para a página anterior */
   function handleNavigateBack() {
     navigation.goBack();
   }
 
-  return (
-    <>
-      <View style={styles.container}>
-        <TouchableOpacity onPress={handleNavigateBack}>
-          <Icon name="arrow-left" size={24} color="#34cb79" />
-        </TouchableOpacity>
+  if (!data.point) {
+    return null;
+  } else {
+    return (
+      <>
+        <View style={styles.container}>
+          <TouchableOpacity onPress={handleNavigateBack}>
+            <Icon name="arrow-left" size={24} color="#34cb79" />
+          </TouchableOpacity>
 
-        <Image
-          style={styles.pointImage}
-          source={{
-            uri:
-              "https://cms.qz.com/wp-content/uploads/2020/04/RTX77LBJ-e1587021603942.jpg?quality=75&strip=all&w=1600&h=900&crop=1",
-          }}
-        />
+          <Image
+            style={styles.pointImage}
+            source={{
+              uri: data.point.image,
+            }}
+          />
 
-        <Text style={styles.pointName}>Mercadão da Esquina</Text>
-        <Text style={styles.pointItems}>
-          Lâmpadas, Baterias, Pilhas e Óleo de cozinha
-        </Text>
+          <Text style={styles.pointName}>{data.point.name}</Text>
+          <Text style={styles.pointItems}>
+            {data.items.map((item) => item.title).join(", ")}
+          </Text>
 
-        <View style={styles.address}>
-          <Text style={styles.addressTitle}>Endereço</Text>
-          <Text style={styles.addressContent}>Alvinópolis, MG</Text>
+          <View style={styles.address}>
+            <Text style={styles.addressTitle}>Endereço</Text>
+            <Text style={styles.addressContent}>
+              {data.point.city}, {data.point.uf}
+            </Text>
+            <Text style={styles.addressContent}>{data.point.email}</Text>
+          </View>
         </View>
-      </View>
 
-      <View style={styles.footer}>
-        <RectButton style={styles.button} onPress={() => {}}>
-          <FontAwesome name="whatsapp" size={24} color="#fff" />
-          <Text style={styles.buttonText}>WhatsApp</Text>
-        </RectButton>
+        <View style={styles.footer}>
+          <RectButton style={styles.button} onPress={() => handleWhatsapp()}>
+            <FontAwesome name="whatsapp" size={24} color="#fff" />
+            <Text style={styles.buttonText}>WhatsApp</Text>
+          </RectButton>
 
-        <RectButton style={styles.button} onPress={() => {}}>
-          <Icon name="mail" size={24} color="#fff" />
-          <Text style={styles.buttonText}>E-mail</Text>
-        </RectButton>
-      </View>
-    </>
-  );
+          <RectButton style={styles.button} onPress={() => {}}>
+            <Icon name="mail" size={24} color="#fff" />
+            <Text style={styles.buttonText}>E-mail</Text>
+          </RectButton>
+        </View>
+      </>
+    );
+  }
 };
 
 export default Detail;
